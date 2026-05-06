5fada2747c723d6cd95c885081f0979f6e92ac7f
Priyanshu Singh <priyanshu7829singh@gmail.com>
2026-05-06 13:51:58 +0530

    chore: added vercel speed insights

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err) => console.error(err));
